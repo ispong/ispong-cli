9580206e7a244b6b87f81cb13e9e7811cc4ecd51
ispong <ispong@outlook.com>
2023-12-14 19:14:03 +0800

    :sparkles: 添加自动打包action

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo wget https://openfly.oss-cn-shanghai.aliyuncs.com/isx/isx_darwin_arm64 -O /usr/local/bin/i
+sudo wget https://github.com/ispong/ispong-cli/releases/download/v1.0.0/isx_darwin_arm64 -O /usr/local/bin/i
 sudo chmod a+x /usr/local/bin/i
 
 echo "安装成功"
